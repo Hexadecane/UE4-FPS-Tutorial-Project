a0b5356d5083d9d7904bf9d06ba321eeb00302ac
Hexadecane <36948730+Hexadecane@users.noreply.github.com>
2022-04-11 22:50:24 -0400

    Add mouselook

--- Source/FPSTutorialProject/FPSCharacter.cpp
+++ Source/FPSTutorialProject/FPSCharacter.cpp
@@ -38,6 +38,9 @@ void AFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompon
 	// Set up "movement" bindings.
 	PlayerInputComponent->BindAxis("MoveForward", this, &AFPSCharacter::MoveForward);
 	PlayerInputComponent->BindAxis("MoveRight", this, &AFPSCharacter::MoveRight);
+	// Set up "look" bindings.
+	PlayerInputComponent->BindAxis("Turn", this, &AFPSCharacter::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &AFPSCharacter::AddControllerPitchInput);
 }
 
 void AFPSCharacter::MoveForward(float Value)
